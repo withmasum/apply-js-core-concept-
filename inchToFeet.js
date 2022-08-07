249bc2f739b476072f8635bb5e33e535131a8a12
Masum Ahmed <creatormasum@gmail.com>
2022-08-08 04:18:34 +0600

    miles-to-km-js

--- inchToFeet.js
+++ inchToFeet.js
@@ -23,6 +23,15 @@ console.log('mamaFeet', mamaFeet); */
 
 function inchToFeet(inches) {
 
-
+    const feet = inches / 12;
+    return feet;
 
 }
+const dadaInches = 144;
+const dadaFeet = inchToFeet(dadaInches);
+console.log(dadaFeet);
+
+
+const nanaInches = 168;
+const nanaFeet = inchToFeet(nanaInches);
+console.log('nana feet', nanaFeet);
---
+++ milesTokm.js
@@ -0,0 +1,13 @@
+function milesToKilometer(miles) {
+
+
+
+    const kilometer = miles * 1.609;
+    return kilometer;
+
+
+}
+
+const mymensinghToDhaka = 190;
+const theKilometerIs = milesToKilometer(mymensinghToDhaka);
+console.log('my killo is', theKilometerIs);
