1e5a11cf50e44cfa0281326c1a8bd54c8a00ecf7
Masum Ahmed <creatormasum@gmail.com>
2022-08-08 04:09:57 +0600

    converter

---
+++ inchToFeet.js
@@ -0,0 +1,28 @@
+/* const myInches = 12;
+const myFeet = myInches / 12;
+console.log(myFeet);
+
+
+const dataInches = 144;
+const dadaFeet = dataInches / 12;
+console.log('data feet', dadaFeet);
+
+const dadiInches = 60;
+
+const dadiFeet = dadiInches / 12;
+console.log('dadiFeet', dadiFeet);
+
+const mamaInches = 360;
+var feetMeasure = 12;
+
+const mamaFeet = mamaInches / feetMeasure;
+console.log('mamaFeet', mamaFeet); */
+
+// Another function method 
+
+
+function inchToFeet(inches) {
+
+
+
+}
