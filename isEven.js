fc7f64c36268b079d25cf8a142b20e196bea2211
Masum Ahmed <creatormasum@gmail.com>
2022-08-08 04:31:03 +0600

    isEvent.js

---
+++ isEven.js
@@ -0,0 +1,42 @@
+/* console.log(4 / 2);
+console.log(98 / 2);
+console.log(144 / 2);
+console.log(1286 / 2); */
+
+
+/* 
+console.log(7 / 2);
+console.log(83 / 2);
+console.log(145 / 2);
+console.log(1287 / 2);
+ */
+
+
+/* 
+console.log(4 % 2);
+console.log(98 % 2);
+console.log(144 % 2);
+console.log(1286 % 2);
+
+
+console.log(7 % 2);
+console.log(83 % 2);
+console.log(145 % 2);
+console.log(1287 % 2);
+ */
+
+
+
+
+function isEven(number) {
+    const reminder = number % 2;
+    console.log(reminder);
+    if (reminder === 0) { console.log('number is even | নাম্বারটি জোর '); }
+
+
+    else {
+        console.log('number is odd | নাম্বারটি বিজোড়');
+    }
+}
+isEven(483);
+isEven(888);
