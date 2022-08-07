77deb4e54e3de5f00e627e0eea1e0e9cec3414bd
Masum Ahmed <creatormasum@gmail.com>
2022-08-08 03:59:42 +0600

    var-let-const.js-finish

---
+++ var-let-const.js
@@ -0,0 +1,7 @@
+let age = 11;
+// let age = 56; XXSame Name isn't using the constant 
+age = 12;
+
+const countryName = 'Bangladesh';
+// countryName = 'chad'; XX Can't using the same name as a constant 
+console.log(age, countryName);
