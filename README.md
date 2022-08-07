a98cad20f8d923dff00370f1ad8aa0f9760541ab
Masum Ahmed <creatormasum@gmail.com>
2022-08-08 05:49:15 +0600

    factorial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # apply-js-core-concept-
+
+
+
+## Need to know more about factiorial on the multiplicationOfNumber.js files 
---
+++ multiplicationOfNumbers.js
@@ -0,0 +1,33 @@
+// 1 * 2 * 3 * 4 *5 *6 * 7
+function factorial(numbers) {
+
+    let result = 1;
+    for (let i = 1; i <= 7; i++) {
+
+        result = result * i;
+
+    }
+
+    return result;
+}
+
+const result = factorial(7);
+console.log(result);
+
+//Factoriral 3! = 3*2*1 
+// 4! = 4*3*2*1 
+
+function factorial(numbers) {
+
+    let result = 1;
+    for (let i = 1; i <= 7; i++) {
+
+        result = result * i;
+
+    }
+
+    return result;
+}
+
+const result = factorial(7);
+console.log(result);
