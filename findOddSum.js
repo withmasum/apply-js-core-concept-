75ab71ba125376b92e53826e71772df55850ca3c
Masum Ahmed <creatormasum@gmail.com>
2022-08-08 05:39:26 +0600

    sum

--- findOddSum.js
+++ findOddSum.js
@@ -1,4 +1,4 @@
-function getSumOfAnArray(numbers) {
+/* function getSumOfAnArray(numbers) {
     // Primary let value is 0 zero; 
     let sum = 0;
     for (let i = 0; i < numbers.length; i++) {
@@ -15,3 +15,31 @@ function getSumOfAnArray(numbers) {
 const myNumbers = [12, 65, 45, 78, 32, 45, 91];
 
 getSumOfAnArray(myNumbers);
+ */
+
+
+
+//get odd numbers of an arry 
+
+
+function getOddNumbersOfAnAray(numbers) {
+    const oddNumbers = [];
+    for (let i = 0; i < numbers.length; i++) {
+        const index = i;
+
+        const element = numbers[index];
+        if (element % 2 === 1) {
+            console.log(index, element);
+            oddNumbers.push(element);
+
+        }
+
+    }
+
+    return oddNumbers;
+}
+
+const myNumbers = [12, 65, 45, 78, 32, 45, 91];
+const oddNumbers = getOddNumbersOfAnAray(myNumbers)
+console.log(oddNumbers);
+
