457ddb7e53f6014fb10ca23926c1b5ece560609d
Masum Ahmed <creatormasum@gmail.com>
2022-08-08 03:55:22 +0600

    finish-recap.js

--- recap.js
+++ recap.js
@@ -5,4 +5,54 @@ var myName = 'khan';
 // shorthand: +=, --, *=, /=
 // ==, --
 
+// 2. Array 
+var firneds = ['abul', 'babul', 'cabul', 'dabul', 'ebul'];
+
+//index 
+
+var thirdFriend = frineds[2];
+firneds[3] = 'dambul';
+
+// 3. Conditionsls
+// >, <, >=, <=, ===, !=, !==
+if (frineds.length < 3) {
+    console.log('fokir tor kon friend nai')
+}
+
+else {
+
+}
+
+// 4. Loop
+var number = 0;
+while (number <= 6) {
+    console.log(number);
+    number++;
+}
+
+
+// for loop 
+
+for (var i = 0; i <= 10; i++) {
+
+
+
+}
+
+// 5. Function 
+
+function isMoonUp(time) {
+    if (time > 7) {
+        return true;
+    }
+}
+
+// 6.Object 
+
+var jantus = {
+    height: 60,
+    romanticism: 'heavvy',
+    bappErTaka: 'borolox'
+
+}
 
