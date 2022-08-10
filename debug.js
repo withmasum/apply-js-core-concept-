5f479bcd63a71b14eee34559cdc99b3065ffb485
Masum Ahmed <creatormasum@gmail.com>
2022-08-11 04:26:05 +0600

    hello

--- debug.js
+++ debug.js
@@ -14,3 +14,9 @@ function getFactorial(number) {
 
 const factorial = getFactorial(12);
 console.log('factorial of 12 is:', factorial);
+function print(a, b, c) {
+    return a + 2;
+    return a * b;
+    return b * c + a;
+}
+console.log(print(1, 2, 3));
